4f56dfd214c9f0f72dde730c067872332966cefd
woldesinbet <lealem.abera@yahoo.com>
2015-02-23 16:53:19 -0600

    Fizz Buzz Program

---
+++ W1D1/working FizzBuzz/src/Buzz12.java
@@ -0,0 +1,22 @@
+
+public class Buzz12 {
+
+	public static void main(String[] args) {
+	 for(int x=0; x<=100; x++){
+		 if(x%3==0)
+			 System.out.println("FIZZ");
+		 if(x%5==0)
+			 System.out.println("BUZZ");
+		 
+		 if(x%3==0&&x%5==0)
+			 System.out.println("FIZZ BUZZ ");
+		 else 
+			 System.out.println(x);
+			 
+	 }
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
