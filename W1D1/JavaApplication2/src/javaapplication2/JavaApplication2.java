ac850d1b556b35144a7b3ea560544fc9a89df9b6
woldesinbet <lealem.abera@yahoo.com>
2015-02-23 15:40:37 -0600

    breersong

---
+++ W1D1/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author lealemmum
+ */
+public class JavaApplication2 {
+ int beerNum=99;
+		String word="botteles";
+		while (beerNum>0){
+			if (beerNum==1){
+				word="bottle";
+			}
+			System.out.println(beerNum + word +"of bree on the wall");
+			System.out.println(beerNum+word+"of bree.");
+			System.out.println("take one down.");
+			System.out.println("pass it around");
+			beerNum=beerNum-1;
+			if (beerNum>0){
+				System.out.println(beerNum+"  "+"of bree on the wall");
+			}else{
+					System.out.println("no more bottle of bree on the wall");
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
